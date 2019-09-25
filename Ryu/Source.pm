b1895f32b258964369d899bc543fba968e6507b8
Amin Marashi <marashi.s.amin@gmail.com>
2019-09-25 11:49:00 +0800

    Remove the implementations

--- Ryu/Source.pm
+++ Ryu/Source.pm
@@ -25,93 +25,30 @@ sub create_source {
 
 # Emit new items to the source
 sub emit {
-    my ($self, $item) = @_;
-
-    return unless defined $self->{listeners};
-
-    $_->($item) for $self->{listeners}->@*;
 }
 
 # Listen on items reaching the source
 sub each {
-    my ($self, $code) = @_;
-
-    push $self->{listeners}->@*, $code;
-
-    return $self;
 }
 
 # Skip a few items from the source
 sub skip {
-    my ($self, $count) = @_;
-
-    return $self unless $count;
-
-    my $source = $self->create_source(label => 'skip');
-
-    $self->each(sub {
-        $source->emit(shift) unless $count-- > 0;
-    });
-
-    return $source;
 }
 
 # Returns items and their index: [$item, $idx]
 sub with_index {
-    my $self = shift;
-
-    my $source = $self->create_source(label => 'with_index');
-
-    my $idx = 0;
-    $self->each(sub {
-        my $item = shift;
-        $source->emit([$item, $idx++]);
-    });
-
-    return $source;
 }
 
 # Similar to map function, changes a received item based on a coderef
 sub map {
-    my ($self, $code) = @_;
-
-    my $source = $self->create_source(label => 'map');
-
-    $self->each(sub {
-        my $item =shift;
-        $source->emit($code->($item));
-    });
-
-    return $source;
 }
 
 # Filter items based on a regex
 sub filter {
-    my ($self, $code) = @_;
-
-    my $source = $self->create_source(label => 'filter');
-
-    $self->each(sub {
-        my $item = shift;
-        $source->emit($item) if $code->($item);
-    });
-
-    return $source;
 }
 
 # Return distinct values, duplicate values are dropped
 sub distinct {
-    my ($self, $code) = @_;
-
-    my $source = $self->create_source(label => 'distinct');
-
-    my %seen;
-    $self->each(sub {
-        my $item = shift;
-        $source->emit($item) unless $seen{$item}++;
-    });
-
-    return $source;
 }
 
 #############################################################################
